c1562873b3ebfa6c87664cc7a8007beae137adef
Simon the Grey <puresamari@gmail.com>
2024-01-21 15:45:06 +0100

    Release maybe

--- src/std_mods/osc.h
+++ src/std_mods/osc.h
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <math.h>
 
+// They dropped it in c99 for some reaason: https://ubuntuforums.org/showthread.php?t=583094
+#ifndef M_PI
+#define M_PI           3.14159265358979323846
+#endif
+
 #include "../module.h"
 #include "../config.h"
 
